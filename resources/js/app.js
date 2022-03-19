0b2a86ed11b18c35fe85452c32ace47305021ac5
Ashraful Islam <ashrafulislam@Ashrafuls-MacBook-Pro.local>
2022-03-19 16:22:53 +0600

    Laravel Vue TailwindCSS Boilerplate setup

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+import { createApp } from "vue";
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import router from "./router";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(VueAxios, axios);
+
+app.mount("#app");
